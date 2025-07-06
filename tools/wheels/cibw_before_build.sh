8976359472753737180da115077ed96739eb11d2
Ralf Gommers <ralf.gommers@gmail.com>
2025-07-06 17:39:33 +0200

    Clean up the missing around with DYLD_LIBRARY_PATH

--- tools/wheels/cibw_before_build.sh
+++ tools/wheels/cibw_before_build.sh
@@ -42,18 +42,6 @@ if [[ "$INSTALL_OPENBLAS" = "true" ]] ; then
     mkdir -p $PKG_CONFIG_PATH
     python -m pip install -r $PROJECT_DIR/requirements/openblas_requirements.txt
     python -c "import scipy_${OPENBLAS}; print(scipy_${OPENBLAS}.get_pkg_config())" > $PKG_CONFIG_PATH/scipy-openblas.pc
-    # Copy the shared objects to a path under $PKG_CONFIG_PATH, the build
-    # will point $LD_LIBRARY_PATH there and then auditwheel/delocate will
-    # pull these into the wheel. Use python to avoid windows/posix problems
-    python <<EOF
-import os, scipy_${OPENBLAS}, shutil
-srcdir = os.path.join(os.path.dirname(scipy_${OPENBLAS}.__file__), "lib")
-shutil.copytree(srcdir, os.path.join("$PKG_CONFIG_PATH", "lib"))
-srcdir = os.path.join(os.path.dirname(scipy_${OPENBLAS}.__file__), ".dylibs")
-if os.path.exists(srcdir):  # macosx delocate
-    shutil.copytree(srcdir, os.path.join("$PKG_CONFIG_PATH", ".dylibs"))
-EOF
-    # pkg-config scipy-openblas --print-provides
 fi
 
 # cibuildwheel doesn't install delvewheel by default (it does install
