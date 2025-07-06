f4b276dab6b9946179055766049ddb1918266269
Ralf Gommers <ralf.gommers@gmail.com>
2025-07-06 16:50:20 +0200

    MAINT: minor cleanups in wheels.yml

--- tools/wheels/cibw_before_build.sh
+++ tools/wheels/cibw_before_build.sh
@@ -4,9 +4,6 @@ PROJECT_DIR="${1:-$PWD}"
 NUMPY_SRC_DIR="${1:-$PWD}/numpy-src"
 
 
-# remove any cruft from a previous run
-rm -rf build
-
 # Update license
 echo "" >> $NUMPY_SRC_DIR/LICENSE.txt
 echo "----" >> $NUMPY_SRC_DIR/LICENSE.txt
@@ -28,29 +25,25 @@ elif [ -z $INSTALL_OPENBLAS ]; then
     export INSTALL_OPENBLAS=true
 fi
 
-# Install Openblas from scipy-openblas64
+# Install OpenBLAS from scipy-openblas64
 if [[ "$INSTALL_OPENBLAS" = "true" ]] ; then
-    # by default, use scipy-openblas64
+    # By default, use scipy-openblas64
+    # On 32-bit platforms and on win-arm64, use scipy-openblas32
     OPENBLAS=openblas64
-    # Possible values for RUNNER_ARCH in github are
-    # X86, X64, ARM, or ARM64
-    # TODO: should we detect a missing RUNNER_ARCH and use platform.machine()
-    #    when wheel build is run outside github?
-    # On 32-bit platforms, use scipy_openblas32
-    # On win-arm64 use scipy_openblas32
+    # Possible values for RUNNER_ARCH in GitHub Actions are: X86, X64, ARM, or ARM64
     if [[ $RUNNER_ARCH == "X86" || $RUNNER_ARCH == "ARM" ]] ; then
         OPENBLAS=openblas32
     elif [[ $RUNNER_ARCH == "ARM64" && $RUNNER_OS == "Windows" ]] ; then
         OPENBLAS=openblas32
     fi
-    echo PKG_CONFIG_PATH is $PKG_CONFIG_PATH, OPENBLAS is ${OPENBLAS}
     PKG_CONFIG_PATH=$PROJECT_DIR/.openblas
+    echo PKG_CONFIG_PATH is $PKG_CONFIG_PATH, OPENBLAS is ${OPENBLAS}
     rm -rf $PKG_CONFIG_PATH
     mkdir -p $PKG_CONFIG_PATH
     python -m pip install -r $PROJECT_DIR/requirements/openblas_requirements.txt
     python -c "import scipy_${OPENBLAS}; print(scipy_${OPENBLAS}.get_pkg_config())" > $PKG_CONFIG_PATH/scipy-openblas.pc
     # Copy the shared objects to a path under $PKG_CONFIG_PATH, the build
-    # will point $LD_LIBRARY_PATH there and then auditwheel/delocate-wheel will
+    # will point $LD_LIBRARY_PATH there and then auditwheel/delocate will
     # pull these into the wheel. Use python to avoid windows/posix problems
     python <<EOF
 import os, scipy_${OPENBLAS}, shutil
@@ -62,7 +55,9 @@ if os.path.exists(srcdir):  # macosx delocate
 EOF
     # pkg-config scipy-openblas --print-provides
 fi
+
+# cibuildwheel doesn't install delvewheel by default (it does install
+# auditwheel on Linux and delocate on macOS)
 if [[ $RUNNER_OS == "Windows" ]]; then
-    # delvewheel is the equivalent of delocate/auditwheel for windows.
-    python -m pip install delvewheel wheel
+    python -m pip install delvewheel
 fi
